d6c3512469b0ea9ae791ddfe55cb20ec66285564
Joel Martin <34744152+pyronitrex@users.noreply.github.com>
2022-10-08 15:50:10 -0400

    color

--- dp-react/src/components/sections/Hero.js
+++ dp-react/src/components/sections/Hero.js
@@ -69,7 +69,7 @@ const Hero = ({
                 </p>
               <div className="reveal-from-bottom" data-reveal-delay="600">
                 <ButtonGroup>
-                  <Button tag="a" color="primary" wideMobile href="https://cruip.com/">
+                  <Button tag="a" color="founder" wideMobile href="https://cruip.com/">
                     Get started
                     </Button>
                   <Button tag="a" color="dark" wideMobile href="https://github.com/cruip/open-react-template/">
